04ad202751d59e9b2c03a79d9a1646af9c2008fa
davepandit <debajyotipandit8@gmail.com>
2025-10-30 23:15:24 +0530

    Made deployment fix

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -5,7 +5,7 @@ config();
 
 const PORT = process.env.PORT || 5000;
 connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log("Server is running on http://localhost:" + PORT);
-  });
+  app.listen(PORT, "0.0.0.0", () =>
+    console.log(`Server is running on port ${PORT}`)
+  );
 });
